9a3e18dfbefc0685f82f29361b75514bfcfd3939
Kenichiro Kurihara <kuriken@gmail.com>
2017-09-03 09:34:58 -0700

    Got tm1638 example code to work.  Updates readme.

--- FeatOfStrength.ino
+++ FeatOfStrength.ino
@@ -1,29 +1,48 @@
-/* LED Blink, Teensyduino Tutorial #1
-   http://www.pjrc.com/teensy/tutorial.html
+/*
+Library examples for TM1638.
 
-   This example code is in the public domain.
+Copyright (C) 2011 Ricardo Batista <rjbatista at gmail dot com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the version 3 GNU General Public License as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-// Teensy 2.0 has the LED on pin 11
-// Teensy++ 2.0 has the LED on pin 6
-// Teensy 3.x / Teensy LC have the LED on pin 13
-const int ledPin = 13;
+#include <TM1638.h>
 
-// the setup() method runs once, when the sketch starts
+const byte dioPin = 8;
+const byte clkPin = 9;
+const byte stb0Pin = 7;
+const bool isDisplayActive = true;
+const byte intensity = 1;
 
-void setup()
+TM1638 module(dioPin, clkPin, stb0Pin, isDisplayActive, intensity);
+
+void setup() 
 {
-  // initialize the digital pin as an output.
-  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+
+  // display a hexadecimal number and set the left 4 dots
+  module.setDisplayToHexNumber(0x1234ABCD, 0xF0);
 }
 
-// the loop() methor runs over and over again,
-// as long as the board has power
-void loop()
+void loop() 
 {
-  digitalWrite(ledPin, HIGH);   // set the LED on
-  delay(500);                  // wait for a second
-  digitalWrite(ledPin, LOW);    // set the LED off
-  delay(500);                  // wait for a second
-}
+  // light the first 4 red LEDs and the last 4 green LEDs as the buttons are pressed
+  byte keys = module.getButtons();
+  module.setLEDs(((keys & 0xF0) << 8) | (keys & 0xF));
 
+  // Print keys
+  Serial.print("keys: ");
+  Serial.println(keys, HEX);
+
+  delay(1000);
+}
